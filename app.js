3e00680aaa608e7d8a0d9a173d6203ed41dce374
lutkenhaus <gustavomayron9@gmail.com>
2023-12-04 11:57:54 -0300

    CORS protocol

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var dotenv = require('dotenv').config({path: __dirname + '/.env'})
 var cookieParser = require('cookie-parser');
@@ -23,6 +24,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
